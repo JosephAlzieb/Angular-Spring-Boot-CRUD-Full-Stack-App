aaf108034888b37efb742922510e935c9b40f1b9
JosephAlzieb <alzieb@iwkoeln.de>
2025-08-30 15:10:58 +0200

    Init - Backend Springboot

---
+++ Employee-Managment-Backend/src/main/java/com/example/employee/management/EmployeeManagmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagmentBackendApplication.class, args);
+	}
+
+}
