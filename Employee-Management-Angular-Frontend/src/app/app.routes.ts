ab677bda4d259054d87f3ac5d4212e39ccbc0f71
JosephAlzieb <alzieb@iwkoeln.de>
2025-09-01 22:56:16 +0200

    fix: change render mode to server for all routes in app.routes.server.ts

--- Employee-Management-Angular-Frontend/src/app/app.routes.ts
+++ Employee-Management-Angular-Frontend/src/app/app.routes.ts
@@ -3,18 +3,12 @@ import { EmployeeList } from './components/employee-list/employee-list';
 import { EmployeeCreate } from './components/employee-create/employee-create';
 import { EmployeeUpdate } from './components/employee-update/employee-update';
 import { EmployeeDetail } from './components/employee-detail/employee-detail';
-import { RenderMode } from '@angular/ssr';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const routes: Routes = [
     { path: 'employees', component: EmployeeList },
     { path: 'create', component: EmployeeCreate },
     { path: '', redirectTo: 'employees', pathMatch: 'full' },
-    { path: 'update/:id', component: EmployeeUpdate, title: 'Update Employee', 
-        data: { renderMode: RenderMode.Server }
-
-    },
-    { path: 'details/:id', component: EmployeeDetail, title: 'Employee Details',
-        data: { renderMode: RenderMode.Server }
-
-    },
+    { path: 'update/:id', component: EmployeeUpdate, title: 'Update Employee' },
+    { path: 'details/:id', component: EmployeeDetail, title: 'Employee Details' },
 ];
